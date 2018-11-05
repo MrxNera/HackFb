989a22d15aae180ac64970e1898478903250f187
MrxNera <44729739+MrxNera@users.noreply.github.com>
2018-11-05 10:29:26 +0700

    Add files via upload

---
+++ HACKFB.sh
@@ -0,0 +1,116 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+cd module
+python2 log.py
+echo ""
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $yellow "Tungguin$green sabar ya sayang"
+sleep 3
+clear
+echo $red "L"
+sleep 0.3
+clear
+echo $blue "LO"
+sleep 0.3
+clear
+echo $green "LOA"
+sleep 0.3
+clear
+echo $cyan "LOAD"
+sleep 0.3
+clear
+echo $white "LOADI"
+sleep 0.3
+clear
+echo $yellow "LOADIN"
+sleep 0.3
+clear
+echo $red "LOADING"
+sleep 0.3
+clear
+echo $green "WELCOME TO MY TOOL Mr,X TEAM"
+sleep 3
+clear
+echo
+toilet -f standard " =* HACKFB *= " -F gay
+echo
+echo ""
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
+echo $blue"[Author    ]: N3R4"
+echo $yellow"[type     ]: Hack Facebook"
+echo $red"[Team     ]: Mr.X Team"
+echo $purple"[Thanks To All Member]"
+echo $green"[Special Thanks to]: Swity"
+echo "\033[34;1m[^_^]\033[36;1m===============================================\033[34;1m[^_^]"
+echo "\033[33;1m error 404"
+echo "\033[32;1m APUSIN DULU SEMUA BOKEP LU!!!"
